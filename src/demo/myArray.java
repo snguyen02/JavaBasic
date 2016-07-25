353e8ab69deb6bd5b51a13c59e93222e5da6e6e4
son nguyen <sonnguyen02@gmail.com>
2016-07-25 22:06:09 +0700

    arr

---
+++ src/demo/myArray.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class myArray {
+	public static int main(String[] args){
+		int[] arr = new int[3];
+		arr[1] =  3;
+		arr[2] = 14;
+		arr[3] = 21;
+		
+				for (int i=0;  i<3;i++){
+					System.out.println("Gia tri:"+ i);
+				}
+		return 1;
+	}
+
+}
