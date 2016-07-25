1edd9235a85ea7312da895ba814c8b602dc41362
son nguyen <sonnguyen02@gmail.com>
2016-07-25 21:44:15 +0700

    25

---
+++ src/demo/Toantu.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Toantu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 8;
+		int b = 10;
+		int ketqua =  a + b;
+		System.out.print("Ket Qua: "+ ketqua);
+		
+
+	}
+
+}
