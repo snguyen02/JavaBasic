97b1efbc75cc444511f9a8f7002933c9cdc083cc
son nguyen <sonnguyen02@gmail.com>
2016-07-25 21:56:27 +0700

    scan

---
+++ src/demo/myScanner.java
@@ -0,0 +1,15 @@
+package demo;
+import java.util.Scanner;
+
+public class myScanner {
+	public static void main(String[] args){
+		
+		Scanner Scan = new Scanner(System.in);
+		System.out.println("Nhap vao");
+		String Nhap = Scan.nextLine();
+		System.out.println("Ket Qua:"+ Nhap);
+		
+	}
+		
+	}
+
