0500be242797e6643b68a9f15434b05618122d4e
son nguyen <sonnguyen02@gmail.com>
2016-07-25 22:23:15 +0700

    str

---
+++ src/demo/StringArray.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class StringArray {
+
+	public static void main(String[] args){
+		String[] str = new String[10];
+		str[0] = "Hello";
+		str[1] = "My Class";
+		for(int i = 0; i<str.length; i++){
+			
+			System.out.println(i);
+		
+		}
+		}
+	
+	}
+
